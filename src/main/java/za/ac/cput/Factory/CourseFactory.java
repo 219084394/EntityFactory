a68ef80b98268b069e4fea14a220cfcbe3653853
Keenan <219264228@mycput.ac.za>
2021-06-09 12:21:30 +0200

    CourseFactory and DepartmentFactory committed

--- src/main/java/za/ac/cput/Factory/CourseFactory.java
+++ src/main/java/za/ac/cput/Factory/CourseFactory.java
@@ -1,4 +1,16 @@
 package za.ac.cput.Factory;
 
+import za.ac.cput.Entity.Course;
+
 public class CourseFactory {
+
+    public static Course courseChosen(String courseCode, String courseName){
+
+        Course course = new Course.Builder()
+                .setCourseCode(courseCode)
+                .setCourseName(courseName)
+                .build();
+
+        return course;
+    }
 }
--- src/main/java/za/ac/cput/Factory/DepartmentFactory.java
+++ src/main/java/za/ac/cput/Factory/DepartmentFactory.java
@@ -1,4 +1,17 @@
 package za.ac.cput.Factory;
 
+import za.ac.cput.Entity.Department;
+
 public class DepartmentFactory {
+
+    public static Department departmentChosen(String depCode, String depName, int officeNo){
+
+        Department department = new Department.Builder()
+                .setDepCode(depCode)
+                .setDepName(depName)
+                .setOfficeNo(officeNo)
+                .build();
+
+        return department;
+    }
 }
