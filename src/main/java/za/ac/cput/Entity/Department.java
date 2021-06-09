3ae2bb4d87664a987dcd945f158c5a9a58c316d2
Keenan <219264228@mycput.ac.za>
2021-06-09 12:16:45 +0200

    Course and Department committed

--- src/main/java/za/ac/cput/Entity/Department.java
+++ src/main/java/za/ac/cput/Entity/Department.java
@@ -1,4 +1,51 @@
 package za.ac.cput.Entity;
 
 public class Department {
+    private String depCode, depName;
+    private int officeNo;
+
+    private Department(Builder builder){
+        this.depCode = builder.depCode;
+        this.depName = builder.depName;
+        this.officeNo = builder.officeNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "depCode='" + depCode + '\'' +
+                ", depName='" + depName + '\'' +
+                ", officeNo=" + officeNo +
+                '}';
+    }
+
+    public static class Builder{
+        private String depCode, depName;
+        private int officeNo;
+
+        public Builder setDepCode(String depCode) {
+            this.depCode = depCode;
+            return this;
+        }
+
+        public Builder setDepName(String depName) {
+            this.depName = depName;
+            return this;
+        }
+
+        public Builder setOfficeNo(int officeNo) {
+            this.officeNo = officeNo;
+            return this;
+        }
+        public Department build(){
+            return new Department(this);
+        }
+        public Builder copy(Department department){
+            this.depCode = department.depCode;
+            this.depName = department.depName;
+            this.officeNo = department.officeNo;
+
+            return this;
+        }
+    }
 }
