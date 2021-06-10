1ccb086c8f9c852676db4dcca0bcfee738b41ab5
Keenan <219264228@mycput.ac.za>
2021-06-10 16:44:22 +0200

    Updated CourseFactory and DepartmentFactory

--- src/main/java/za/ac/cput/Factory/CourseFactory.java
+++ src/main/java/za/ac/cput/Factory/CourseFactory.java
@@ -10,13 +10,11 @@ import za.ac.cput.Entity.Course;
 
 public class CourseFactory {
 
-    public static Course courseChosen(String courseCode, String courseName){
+    public static Course build(String courseCode, String courseName){
 
-        Course course = new Course.Builder()
+        return new Course.Builder()
                 .setCourseCode(courseCode)
                 .setCourseName(courseName)
                 .build();
-
-        return course;
     }
 }
--- src/main/java/za/ac/cput/Factory/DepartmentFactory.java
+++ src/main/java/za/ac/cput/Factory/DepartmentFactory.java
@@ -10,14 +10,12 @@ import za.ac.cput.Entity.Department;
 
 public class DepartmentFactory {
 
-    public static Department departmentChosen(String depCode, String depName, int officeNo){
+    public static Department build(String depCode, String depName, int officeNo){
 
-        Department department = new Department.Builder()
+        return new Department.Builder()
                 .setDepCode(depCode)
                 .setDepName(depName)
                 .setOfficeNo(officeNo)
                 .build();
-
-        return department;
     }
 }
