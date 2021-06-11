8b28d6325c19bfd3ec5b17d7e2e15cc6bf6e8824
219084394 <lcstewart.di@gmail.com>
2021-06-11 17:18:23 +0200

    Pushing for Abdul & Wajedien (reason git push/pull problems)

--- src/main/java/za/ac/cput/Entity/Building.java
+++ src/main/java/za/ac/cput/Entity/Building.java
@@ -1,4 +1,52 @@
 package za.ac.cput.Entity;
 
+/* Wajedien Samuels
+    216287820
+ */
+
 public class Building {
+
+    private String BuildCode, BuildName;
+
+    private Building (Builder builder){
+        this.BuildCode = builder.BuildCode;
+        this.BuildName = builder.BuildName;
+    }
+
+    @Override
+    public String toString() {
+        return "Building{" +
+                "BuildCode='" + BuildCode + '\'' +
+                ", BuildName='" + BuildName + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+
+        private String BuildCode, BuildName;
+
+        public Builder setBuildCode(String buildCode) {
+            this.BuildCode = buildCode;
+
+            return this;
+        }
+
+        public Builder setBuildName(String buildName) {
+            this.BuildName = buildName;
+
+            return this;
+        }
+
+        public Building build() {
+
+            return new Building(this);
+        }
+
+        public Builder copy(Building building) {
+            this.BuildCode = building.BuildCode;
+            this.BuildName = building.BuildName;
+
+            return this;
+        }
+    }
 }
--- src/test/java/za/ac/cput/Entity/BuildingTest.java
+++ src/test/java/za/ac/cput/Entity/BuildingTest.java
@@ -1,7 +1,48 @@
 package za.ac.cput.Entity;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.Entity.Building;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/* Wajedien Samuels
+    216287820
+ */
 class BuildingTest {
+
+    @Test
+    void testEquality(){
+        Building building1 = new Building.Builder().setBuildCode("1000").setBuildName("Tech").build();
+        Building building2 = building1;
+        assertEquals(building2, building1);
+
+    }
+
+    @Test
+    void testIdentity() {
+
+        Building building1 = new Building.Builder().setBuildCode("1000").setBuildName("Tech").build();
+        Building building2 = building1;
+        assertSame(building2, building1);
+    }
+
+    @Disabled
+    @Test
+    void testTimeout() {
+
+        assertTimeout(Duration.ofMinutes(100), () -> {
+            Thread.sleep(2000);
+
+            System.out.println("Test Timed out");
+        });
+    }
+
+
 
 }
