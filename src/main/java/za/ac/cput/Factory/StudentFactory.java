c3351a060d6e88c2a7f886ea049d74ebe606b687
Anicka Schouw <anickaschouw@gmail.com>
2021-06-11 15:08:20 +0200

    Student Factory code added. @author: Anicka Schouw

--- src/main/java/za/ac/cput/Factory/StudentFactory.java
+++ src/main/java/za/ac/cput/Factory/StudentFactory.java
@@ -1,4 +1,6 @@
 package za.ac.cput.Factory;
+import za.ac.cput.Entity.Student;
+
 /*StudentFactory.java
 * Student Factory code
 * @author Anicka Schouw 217284183
@@ -6,6 +8,14 @@ package za.ac.cput.Factory;
  */
 public class StudentFactory {
 
-    public static StudentFactory
+    public static Student build( int studentNo, String stFname, String stLname, String stEmail){
+
+        if(stFname.isEmpty() && stLname.isEmpty()){
+            return null;
+        }
+
+        return new Student.StudentBuilder().setStudentNo(studentNo)
+                .setStFname(stFname).setStLname(stLname).setStEmail(stEmail).build();
+    }
 
 }
--- src/test/java/za/ac/cput/Entity/StudentTest.java
+++ src/test/java/za/ac/cput/Entity/StudentTest.java
@@ -6,6 +6,7 @@ package za.ac.cput.Entity;
  */
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import za.ac.cput.Factory.StudentFactory;
 
 import java.time.Duration;
 
@@ -22,9 +23,10 @@ class StudentTest {
 
     @Test
     void testIdentity(){
-        Student student1 = new Student.StudentBuilder().setStLname("Schouw").build();
-        Student student3 = student1;
-        assertSame(student1,student3);
+        Student student = new Student.StudentBuilder().setStudentNo(217284183).setStFname("Anicka").setStLname("Schouw").setStEmail("217284183@mycput.ac.za").build();
+        System.out.println(student);
+        Student student3 = student;
+        assertSame(student,student3);
     }
 
     @Test
@@ -37,9 +39,10 @@ class StudentTest {
     }
     @Disabled
     @Test
-    void testStudentEntity(){
-        Student student = new Student.StudentBuilder().setStudentNo(217284183).setStFname("Anicka").setStLname("Schouw").setStEmail("217284183@mycput.ac.za").build();
-        System.out.println(student);
+    void testStudentFactory(){
+        Student s = StudentFactory.build(217284183,"Anicka","","");
+        System.out.println(s);
+        assertNotNull(s);
     }
 
 }
